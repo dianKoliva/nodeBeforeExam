b6061366bdecbafc0ddbae97a9acfc01de41a1b7
dianKoliva <dianpretty01@gmail.com>
2022-07-05 21:42:10 +0200

    new changes

--- backend/checker.js
+++ backend/checker.js
@@ -2,18 +2,17 @@
     module.exports = (req, res, next) => {
 
         const token = req.headers.authorization;
-        console.log(token);
-        // try {
-        //     const decoded = jwt.verify(token, "trial");
-        //     req.userData = decoded;
-        //     next();
+        try {
+            const decoded = jwt.verify(token, "trial");
+            req.userData = decoded;
+            next();
 
 
-        // } catch (err) {
-        //     return res.status(401).json({
-        //         message: "auth failed",
-        //         error: err
-        //     })
-        // }
+        } catch (err) {
+            return res.status(401).json({
+                message: "auth failed",
+                error: err
+            })
+        }
 
     }
